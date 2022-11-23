9ca9e178eab952c560fb23fe012721c5e6b52573
chenyulin <chenyulin@sjtu.edu.cn>
2022-11-23 16:30:58 +0800

    fix lsp

--- lua/lsp/lspconfig.lua
+++ lua/lsp/lspconfig.lua
@@ -1,51 +1,77 @@
 local present, lspconfig = pcall(require, "lspconfig")
 
 if not present then
-  return
+    return
 end
 
 
 local M = {}
 
+local runtime_path = vim.split(package.path, ';')
+table.insert(runtime_path, 'lua/?.lua')
+table.insert(runtime_path, 'lua/?/init.lua')
+
 M.capabilities = vim.lsp.protocol.make_client_capabilities()
 
 M.capabilities.textDocument.completion.completionItem = {
-  documentationFormat = { "markdown", "plaintext" },
-  snippetSupport = true,
-  preselectSupport = true,
-  insertReplaceSupport = true,
-  labelDetailsSupport = true,
-  deprecatedSupport = true,
-  commitCharactersSupport = true,
-  tagSupport = { valueSet = { 1 } },
-  resolveSupport = {
-    properties = {
-      "documentation",
-      "detail",
-      "additionalTextEdits",
+    documentationFormat = { "markdown", "plaintext" },
+    snippetSupport = true,
+    preselectSupport = true,
+    insertReplaceSupport = true,
+    labelDetailsSupport = true,
+    deprecatedSupport = true,
+    commitCharactersSupport = true,
+    tagSupport = { valueSet = { 1 } },
+    resolveSupport = {
+        properties = {
+            "documentation",
+            "detail",
+            "additionalTextEdits",
+        },
     },
-  },
 }
 
 lspconfig.sumneko_lua.setup {
-  on_attach = M.on_attach,
-  capabilities = M.capabilities,
+    on_attach = function(client, bufnr)
+        -- 禁用格式化功能，交给专门插件插件处理
+        client.server_capabilities.document_formatting = false
+        client.server_capabilities.document_range_formatting = false
 
-  settings = {
-    Lua = {
-      diagnostics = {
-        globals = { "vim" },
-      },
-      workspace = {
-        library = {
-          [vim.fn.expand "$VIMRUNTIME/lua"] = true,
-          [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
-        },
-        maxPreload = 100000,
-        preloadFileSize = 10000,
-      },
+        local function buf_set_keymap(...)
+            vim.api.nvim_buf_set_keymap(bufnr, ...)
+        end
+
+        -- 绑定快捷键
+        require('keybindings').mapLSP(buf_set_keymap)
+        -- 保存时自动格式化
+        vim.cmd('autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()')
+    end,
+    init_options = {
+        usePlaceholders = true,
     },
-  },
+    capabilities = M.capabilities,
+
+    settings = {
+        runtime = {
+            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT',
+            -- Setup your lua path
+            path = runtime_path,
+        },
+        diagnostics = {
+            -- Get the language server to recognize the `vim` global
+            globals = { 'vim' },
+        },
+        workspace = {
+            -- Make the server aware of Neovim runtime files
+            library = vim.api.nvim_get_runtime_file('', true),
+            checkThirdParty = false,
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {
+            enable = false,
+        },
+    },
 }
 
 return M
