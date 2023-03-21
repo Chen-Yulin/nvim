dde357abf53f26972bd3ad0e1b1e840412b26fc7
Chen-Yulin <chenyulin@sjtu.edu.cn>
2023-03-21 20:54:57 +0800

    modify obsidian dir

--- lua/plugin-config/obsidian.lua
+++ lua/plugin-config/obsidian.lua
@@ -1,5 +1,5 @@
 require("obsidian").setup({
-  dir = "~/OneDrive/obsidian",
+  dir = "~/OneDrive/Common/obsidian",
   completion = {
     nvim_cmp = true, -- if using nvim-cmp, otherwise set to false
   }
