75eb8055d12cbb02d910bf2fd4b7d029a99ec78f
chenyulin <chenyulin@sjtu.edu.cn>
2024-02-11 21:29:01 +0800

    feat. add noice, notify

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -141,6 +141,21 @@ require("lazy").setup(
             -- or leave it empty to use the default settings
             -- refer to the configuration section below
             },
+        },
+        {
+            "folke/noice.nvim",
+            event = "VeryLazy",
+            opts = {
+                -- add any options here
+            },
+            dependencies = {
+                -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+                "MunifTanjim/nui.nvim",
+                -- OPTIONAL:
+                --   `nvim-notify` is only needed, if you want to use the notification view.
+                --   If not available, we use `mini` as the fallback
+                "rcarriga/nvim-notify",
+            }
         }
 
 })
