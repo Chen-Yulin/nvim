a1cf6785b786241b1f71ccedb5b19aa0a1211e91
Chen Yulin <chenyulin@sjtu.edu.cn>
2025-04-10 20:24:54 +0800

    feat. switch to neotree and add file selection feature

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -108,27 +108,27 @@ local pluginKeys = {}
 
 -- nvim-tree
 -- alt + m 键打开关闭tree
-map("n", "<F2>", ":NvimTreeToggle<CR>", opt)
+map("n", "<F2>", ":Neotree toggle<CR>", opt)
 -- 列表快捷键
-pluginKeys.nvimTreeList = {
-	-- 打开文件或文件夹
-	{ key = { "<CR>", "o", "<2-LeftMouse>" }, action = "edit" },
-	-- 分屏打开文件
-	{ key = "v", action = "vsplit" },
-	{ key = "h", action = "split" },
-	-- 显示隐藏文件
-	{ key = "i", action = "toggle_custom" }, -- 对应 filters 中的 custom (node_modules)
-	{ key = ".", action = "toggle_dotfiles" }, -- Hide (dotfiles)
-	-- 文件操作
-	{ key = "R", action = "refresh" },
-	{ key = "a", action = "create" },
-	{ key = "d", action = "remove" },
-	{ key = "r", action = "rename" },
-	{ key = "x", action = "cut" },
-	{ key = "c", action = "copy" },
-	{ key = "p", action = "paste" },
-	{ key = "s", action = "system_open" },
-}
+-- pluginKeys.nvimTreeList = {
+-- 	-- 打开文件或文件夹
+-- 	{ key = { "<CR>", "o", "<2-LeftMouse>" }, action = "edit" },
+-- 	-- 分屏打开文件
+-- 	{ key = "v", action = "vsplit" },
+-- 	{ key = "h", action = "split" },
+-- 	-- 显示隐藏文件
+-- 	{ key = "i", action = "toggle_custom" }, -- 对应 filters 中的 custom (node_modules)
+-- 	{ key = ".", action = "toggle_dotfiles" }, -- Hide (dotfiles)
+-- 	-- 文件操作
+-- 	{ key = "R", action = "refresh" },
+-- 	{ key = "a", action = "create" },
+-- 	{ key = "d", action = "remove" },
+-- 	{ key = "r", action = "rename" },
+-- 	{ key = "x", action = "cut" },
+-- 	{ key = "c", action = "copy" },
+-- 	{ key = "p", action = "paste" },
+-- 	{ key = "s", action = "system_open" },
+-- }
 
 -- Telescope
 vim.keymap.set("n", "<C-g>", ":Telescope help_tags<CR>")
