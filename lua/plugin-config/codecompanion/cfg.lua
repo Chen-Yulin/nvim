883c1ab38c2967d3da3ac434741ac41730637ddf
Chen Yulin <chenyulin@sjtu.edu.cn>
2025-10-25 16:22:47 +0800

    update lsp and codecompanion adapter

--- lua/plugin-config/codecompanion/cfg.lua
+++ lua/plugin-config/codecompanion/cfg.lua
@@ -1,12 +1,16 @@
 require("codecompanion").setup({
 	strategies = {
 		chat = {
-			adapter = "copilot",
-            model = "claude-sonnet-4"
+			adapter = {
+				name = "copilot",
+				model = "claude-sonnet-4",
+			},
 		},
 		inline = {
-			adapter = "copilot",
-            model = "claude-sonnet-4"
+			adapter = {
+				name = "copilot",
+				model = "claude-sonnet-4",
+			},
 		},
 	},
 })
